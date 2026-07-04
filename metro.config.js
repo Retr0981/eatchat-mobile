ce0cda5f17e5bb1dce05b0f50c38afda23d7b400
retr0981 <davidarmah991@gmail.com>
2026-07-04 20:53:14 +0100

    adding better changes

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// Metro configuration for EatChat (Expo Router)
+// Uses the Expo metro-config bundled with the Expo SDK.
+
+const { getDefaultConfig } = require("expo/node_modules/@expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
